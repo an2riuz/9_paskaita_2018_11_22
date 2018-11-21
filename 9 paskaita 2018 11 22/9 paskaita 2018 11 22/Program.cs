bcfef3b3da2e1e9ee471bb13c8a51fdf5330e35c
an2riuz <an2riuz@gmail.com>
2018-11-21 22:40:34 +0200

    9 Paskaita.

---
+++ 9 paskaita 2018 11 22/9 paskaita 2018 11 22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_paskaita_2018_11_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
